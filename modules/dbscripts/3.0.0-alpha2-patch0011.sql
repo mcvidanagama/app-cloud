5aba9e3937b107c444e397a95f65f393f889868c
Amalka Subasinghe <amalkasubasinghe@gmail.com>
2016-08-22 12:28:50 +0530

    adding as-6.0.0-m3 runtime

--- modules/dbscripts/3.0.0-alpha2-patch0011.sql
+++ modules/dbscripts/3.0.0-alpha2-patch0011.sql
@@ -22,3 +22,27 @@
 UPDATE `AC_RUNTIME`
 SET `name` = 'OpenJDK 8 + WSO2 MSF4J 1.0.0 - Deprecated Runtime (will continue to work until September 30th 2016)'
 WHERE `id`= 2;
+
+-- --------------------------------------------------------
+-- Migration - Adding Apache Tomcat 8.0.28 / WSO2 Application Server 6.0.0-M1 runtime deprecate notice
+-- --------------------------------------------------------
+UPDATE `AC_RUNTIME`
+SET `name` = 'Apache Tomcat 8.0.28 / WSO2 Application Server 6.0.0-M1 - Deprecated (will work until 2016/09/30)'
+WHERE `id`= 1;
+
+-- --------------------------------------------------------
+-- Adding Apache Tomcat 8.0.28 / WSO2 Application Server 6.0.0-M3 runtime
+-- --------------------------------------------------------
+INSERT INTO `AC_RUNTIME` (`id`, `name`, `repo_url`, `image_name`, `tag`, `description`) VALUES
+(10, 'Apache Tomcat 8.0.28 / WSO2 Application Server 6.0.0-M3','registry.docker.appfactory.private.wso2.com:5000', 'wso2as', '6.0.0-m3', 'OS:Debian, JAVA Version:8u72');
+
+INSERT INTO `AC_APP_TYPE_RUNTIME` (`app_type_id`, `runtime_id`) VALUES
+(1, 10);
+
+INSERT INTO `AC_RUNTIME_CONTAINER_SPECIFICATIONS` (`id`, `CON_SPEC_ID`) VALUES
+(10, 3),
+(10, 4);
+
+INSERT INTO AC_RUNTIME_TRANSPORT (`transport_id`, `runtime_id`) VALUES
+(3, 10),
+(4, 10);
--- modules/resources/dockerfiles/dockerImages.sh
+++ modules/resources/dockerfiles/dockerImages.sh
@@ -23,26 +23,26 @@
 CURRENT_DIR=`pwd`
 
 #msf4j base image
-docker build --no-cache -t wso2-appcloud/msf4j:1.0.0-base -f $CURRENT_DIR/msf4j/base/1.0.0/Dockerfile.wso2-appcloud-msf4j-1.0.0.base $CURRENT_DIR/msf4j/base/1.0.0/
-docker build --no-cache -t wso2-appcloud/msf4j:2.0.0-base -f $CURRENT_DIR/msf4j/base/2.0.0/Dockerfile.wso2-appcloud-msf4j-2.0.0.base $CURRENT_DIR/msf4j/base/2.0.0/
+#docker build --no-cache -t wso2-appcloud/msf4j:1.0.0-base -f $CURRENT_DIR/msf4j/base/1.0.0/Dockerfile.wso2-appcloud-msf4j-1.0.0.base $CURRENT_DIR/msf4j/base/1.0.0/
+#docker build --no-cache -t wso2-appcloud/msf4j:2.0.0-base -f $CURRENT_DIR/msf4j/base/2.0.0/Dockerfile.wso2-appcloud-msf4j-2.0.0.base $CURRENT_DIR/msf4j/base/2.0.0/
 
 #carbon 4.2.0 base image
-docker build --no-cache -t wso2-appcloud/carbon:4.2.0-base -f $CURRENT_DIR/carbon/base/1.0.0/Dockerfile.wso2-appcloud-carbon-4.2.0.base $CURRENT_DIR/carbon/base/1.0.0/
+#docker build --no-cache -t wso2-appcloud/carbon:4.2.0-base -f $CURRENT_DIR/carbon/base/1.0.0/Dockerfile.wso2-appcloud-carbon-4.2.0.base $CURRENT_DIR/carbon/base/1.0.0/
 
 #php base image
-docker build --no-cache -t wso2-appcloud/php:1.0.0-base -f $CURRENT_DIR/php/base/1.0.0/Dockerfile.wso2-appcloud-php-1.0.0.base $CURRENT_DIR/php/base/1.0.0
+#docker build --no-cache -t wso2-appcloud/php:1.0.0-base -f $CURRENT_DIR/php/base/1.0.0/Dockerfile.wso2-appcloud-php-1.0.0.base $CURRENT_DIR/php/base/1.0.0
 
 #tomcat base image wso2as600-m1
-docker build --no-cache -t wso2-appcloud/wso2as:6.0.0-m1-base -f $CURRENT_DIR/wso2as/base/6.0.0-m1/Dockerfile.wso2-appcloud-wso2as-6.0.0-m1.base $CURRENT_DIR/wso2as/base/6.0.0-m1
+#docker build --no-cache -t wso2-appcloud/wso2as:6.0.0-m1-base -f $CURRENT_DIR/wso2as/base/6.0.0-m1/Dockerfile.wso2-appcloud-wso2as-6.0.0-m1.base $CURRENT_DIR/wso2as/base/6.0.0-m1
 
 #tomcat base image wso2as600-m2
-docker build --no-cache -t wso2-appcloud/wso2as:6.0.0-m2-base -f $CURRENT_DIR/wso2as/base/6.0.0-m2/Dockerfile.wso2-appcloud-wso2as-6.0.0-m2.base $CURRENT_DIR/wso2as/base/6.0.0-m2
+docker build --no-cache -t wso2-appcloud/wso2as:6.0.0-m3-base -f $CURRENT_DIR/wso2as/base/6.0.0-m3/Dockerfile.wso2-appcloud-wso2as-6.0.0-m3.base $CURRENT_DIR/wso2as/base/6.0.0-m3
 
 #jaggery base image
-docker build --no-cache -t wso2-appcloud/jaggery:0.11.0-base -f $CURRENT_DIR/jaggery/base/0.11.0/Dockerfile.wso2-appcloud-jaggery-0.11.0.base $CURRENT_DIR/jaggery/base/0.11.0
+#docker build --no-cache -t wso2-appcloud/jaggery:0.11.0-base -f $CURRENT_DIR/jaggery/base/0.11.0/Dockerfile.wso2-appcloud-jaggery-0.11.0.base $CURRENT_DIR/jaggery/base/0.11.0
 
 #wso2dataservice base image
-docker build --no-cache -t wso2-appcloud/wso2dataservice:3.5.0-base -f $CURRENT_DIR/wso2dataservice/base/3.5.0/Dockerfile.wso2-appcloud-wso2dataservice-3.5.0.base $CURRENT_DIR/wso2dataservice/base/3.5.0
+#docker build --no-cache -t wso2-appcloud/wso2dataservice:3.5.0-base -f $CURRENT_DIR/wso2dataservice/base/3.5.0/Dockerfile.wso2-appcloud-wso2dataservice-3.5.0.base $CURRENT_DIR/wso2dataservice/base/3.5.0
 
 #wso2esb base image
-docker build --no-cache -t wso2-appcloud/wso2esb:5.0.0-base -f $CURRENT_DIR/wso2esb/base/5.0.0/Dockerfile.wso2-appcloud-wso2esb-5.0.0.base $CURRENT_DIR/wso2esb/base/5.0.0
+#docker build --no-cache -t wso2-appcloud/wso2esb:5.0.0-base -f $CURRENT_DIR/wso2esb/base/5.0.0/Dockerfile.wso2-appcloud-wso2esb-5.0.0.base $CURRENT_DIR/wso2esb/base/5.0.0
