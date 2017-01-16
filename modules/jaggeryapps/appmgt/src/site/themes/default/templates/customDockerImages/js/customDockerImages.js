f18374ee7b51fee9f23d237d9290073c1c5ddd1f
mahesh <mahesh@wso2.com>
2017-01-16 09:05:09 +0530

    crating UI form , ajax block and module layer

--- modules/jaggeryapps/appmgt/src/site/themes/default/templates/customDockerImages/js/customDockerImages.js
+++ modules/jaggeryapps/appmgt/src/site/themes/default/templates/customDockerImages/js/customDockerImages.js
@@ -1,3 +1,53 @@
-/**
- * Created by mcrox on 1/9/17.
+/*
+ *
+ *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ * /
  */
+
+
+
+/**
+ *  Adding new image
+ */
+function addNewImage() {
+    // var validator = $("#addDatabaseForm").validate(getValidationOptions());
+    // var formValidated = validator.form();
+    // if (formValidated) {
+    //     $("#add-database").loadingButton({action:'show'});
+        jagg.post("../blocks/customDockerImages//ajax/customDockerImages.jag", {
+            action: "addImageAndCheckSecurity",
+            imageUrl: $("#imageUrl").val().trim()
+        }, function (result) {
+            alert(result);
+            // result = $.trim(result);
+            // result = JSON.parse(result);
+            // if (result.value == 'success') {
+            //     window.location.href = "databases.jag";
+            // } else {
+                jagg.message({content: 'New Image added and queued for security check.', type: 'info', id: 'addnewcustomdockerimage'});
+            // }
+        }, function (jqXHR, textStatus, errorThrown) {
+            jagg.message({
+                             content: jqXHR.responseText,
+                             type: 'error',
+                             id: 'addnewcustomdockerimage',
+                             timeout: 8000
+                         });
+            $("#add-database").loadingButton({action:'hide'});
+        });
+    // }
+}
--- modules/resources/dockerfiles/dockerImages.sh
+++ modules/resources/dockerfiles/dockerImages.sh
@@ -58,3 +58,6 @@ docker build --no-cache -t docker.wso2.com/wso2-cloud/wso2dss:3.5.1-alpine3.4-or
 
 #wso2esb base image
 docker build --no-cache -t docker.wso2.com/wso2-cloud/wso2esb:5.0.0-alpine3.4-oracle-jdk1.8.0 -f $CURRENT_DIR/wso2esb/base/5.0.0/Dockerfile.wso2-appcloud-wso2esb-5.0.0.base $CURRENT_DIR/wso2esb/base/5.0.0
+
+#docker in docker (DinD) base image for custom docker
+docker build --no-cache -t docker.wso2.com/wso2-cloud/custom:dind-base -f $CURRENT_DIR/custom/base/Dockerfile.docker-in-docker-base-image $CURRENT_DIR/custom/base/
