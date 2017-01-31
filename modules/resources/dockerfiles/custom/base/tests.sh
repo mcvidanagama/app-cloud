0f43a147f54d6af3003aae5e841d9e14b11059a6
mahesh <mahesh@wso2.com>
2017-01-31 20:41:22 +0530

    adding status and imageid to response json

--- modules/resources/dockerfiles/custom/base/tests.sh
+++ modules/resources/dockerfiles/custom/base/tests.sh
@@ -20,7 +20,6 @@
 # ------------------------------------------------------------------------
 
 
-
 # wait until container runs
 running_containers=$(docker ps | sed '1d' | awk '{print $NF}')
 while [ -z "$running_containers" ]; do
@@ -29,19 +28,17 @@ sleep 1
 running_containers=$(docker ps | sed '1d' | awk '{print $NF}')
 done
 
-echo "Runing Container : $running_containers"
 
-echo "Starting test run"
+status="passed"
 
-echo "Test-1 performing"
 # Security check 01 : 4.1  - Create a user for the container
 user=$(docker ps --quiet -a | xargs docker inspect --format '{{.Config.User}}')
 test1="pass"
 if [ "$user" = "" ] || [ "$user" = " " ] || [ "$user" = "[]" ]; then
-        test1="fail";
+        test1="fail"
+        status="failed"
 fi
 
-echo "Test-2 performing"
 # Security check 02 : 5.5 Do not mount sensitive host system directories on containers
 volumes=$(docker ps --quiet -a | xargs docker inspect --format 'Volumes={{ .Mounts }}')
 sensitive_dirs='/boot
@@ -57,34 +54,28 @@ for v in $sensitive_dirs; do
 test2="pass"
 if [ $volumes = *"$v"* ]
 then
-        test2="fail";
+        test2="fail"
+        status="failed"
         break;
 fi
 done
 
 
-echo "Test-3 performing"
 # Security check 03 : 5.6 Do not run ssh within containers
 container_name=$(docker ps | sed '1d' |  awk '{print $NF}')
 processes=$(docker exec "$container_name" ps -el 2>/dev/null | grep -c sshd | awk '{print $1}')
 test3="pass"
 if [ "$processes" -ge 1 ]; then
         test3="fail"
+        status="failed"
 fi
 
 
-echo "Tests completed, publishing results..."
-
 # =========================
 # publishing test results
-resultsJson="{\"TEST01\":\"$test1\",\"TEST02\":\"$test2\",\"TEST03\":\"$test3\"}"
-echo "Results json : $resultsJson"
+resultsJson="{\"imageId\":\"$IMAGE_TAG\",\"status\":\"$status\",\"TEST01\":\"$test1\",\"TEST02\":\"$test2\",\"TEST03\":\"$test3\"}"
 loginEndPoint="site/blocks/user/login/ajax/login.jag"
-echo "Login Endpoint $loginEndPoint"
 adminEndPoint="site/blocks/admin/admin.jag"
-echo "Admin Endpoint $adminEndPoint"
 curl -c cookies -v -X POST -k $APPCLOUD_URL$loginEndPoint -d "action=login&userName=$ADMIN_USERNAME&password=$ADMIN_PASSWORD"
 
 curl -b cookies  -v -X POST -k $APPCLOUD_URL$adminEndPoint -d "action=publishDockerSecurityTestResults&testResultsJson=$resultsJson"
-
-echo "Done...!!!!"
